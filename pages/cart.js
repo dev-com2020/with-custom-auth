3727d05e6903b2900834d9b03cf0558beba6a2c3
dev-com2022 <kwp@tlen.pl>
2023-05-19 15:33:52 +0200

    15:33 19.05

---
+++ pages/cart.js
@@ -0,0 +1,31 @@
+import { useContext, useEffect,useState } from "react";
+import { Box, Flex, Heading, Text, Button, Image, Divider } from "@chakra-ui/react";
+import { CartContext } from "../context/CartContext";
+import getProductsById from "../lib/graphql/queries/getProductsById";
+
+export default function Cart() {
+    const { items } = useContext(CartContext);
+    const [products, setProducts] = useState([]);
+    const hasProducts = Object.keys(items).length
+    useEffect(() => {
+        if (!hasProducts) return;
+    graphql.request(getProductsById, { 
+        ids: Object.keys(items) 
+    })
+        .then((data) => { setProducts(data.products)
+    })
+    .catch((err) => console.log(err));
+},[JSON.stringify(products)]);
+
+
+
+    return (
+        <Box>
+            <Text>Cart</Text>
+            <Divider my={4} />
+            <Box>
+                <Text>Cart Items</Text>
+                </Box>
+        </Box>
+    )
+}
