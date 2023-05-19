91bd075c57e554f30a8f2ea6df17a0ff53684439
dev-com2022 <kwp@tlen.pl>
2023-05-19 11:02:59 +0200

    11:02 19.05

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,7 +1,16 @@
 import graphql from "../../lib/graphql";
 import getAllProducts from "../../lib/graphql/queries/getAllProducts";
+import GetProductBySlug from "../../lib/graphql/queries/getProductBySlug";
 
-export async function getStaticProps() {}
+export async function getStaticProps() {
+    const { products } = await graphql.request(GetProductBySlug,
+        { slug: params.slug, });
+return {
+    props: {
+    product: products[0],
+    },
+    };
+}
 export async function getStaticPaths() {
     const { products } = await graphql.request(getAllProducts);
     return {
